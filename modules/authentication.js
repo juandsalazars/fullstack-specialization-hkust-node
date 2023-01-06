50a894e184c0a349a54297a26a75a1bcf3f826da
Juan David Salazar Santa <juandsalazars@gmail.com>
2023-01-05 20:51:36 -0500

    Passport

--- modules/authentication.js
+++ modules/authentication.js
@@ -1,20 +1,12 @@
 function auth(req, res, next) {
     
-    if (!req.session.user) {
+    if (!req.user) {
         var err = new Error('You are not authenticated!');
         err.status = 403;
         return next(err);
     }
     else {
-        if (req.session.user === 'authenticated') {
-            next();
-        }
-        else {
-            var err = new Error('You are not authenticated!');
-            
-            err.status = 403;
-            return next(err);
-        }
+        next();
     }
 }
 
