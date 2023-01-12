e7c5c8e5a3c99afc0a82106b7015f8851f8f3fa6
Juan David Salazar Santa <juandsalazars@gmail.com>
2023-01-12 12:49:47 -0500

    OAuth 2.0 - Passport - Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '719907242869250',
+        clientSecret: '53ea2479683131946e56d7eb4921672c',
+    }
 }
